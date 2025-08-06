97cd82a8419ddc545dd75abc7e3d00a632702668
Diep Gia Huy <81817335+22127475@users.noreply.github.com>
2025-08-06 10:35:17 +0700

    upload script for create ecs task

---
+++ script/create_ecs_task.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 🟡 Lấy giá trị từ CloudFormation exports
+CLUSTER_NAME="linux-and-application"
+SUBNET_1=$(aws cloudformation list-exports --query "Exports[?Name=='Project01-Public-Subnet-1a'].Value" --output text)
+SUBNET_2=$(aws cloudformation list-exports --query "Exports[?Name=='Project01-Public-Subnet-1b'].Value" --output text)
+SECURITY_GROUP=$(aws cloudformation list-exports --query "Exports[?Name==Project01-App-Tier-SG-ID''].Value" --output text)
+
+# 🟡 Các biến còn lại
+REGION="us-east-1"
+IMAGE_TAG="$1"
+DOCKER_IMAGE="22127475/jenkinsapp:${IMAGE_TAG}"
+TASK_DEF_NAME="jenkins-app-${IMAGE_TAG}"
+
+if [ -z "$IMAGE_TAG" ]; then
+  echo "❌ Thiếu IMAGE_TAG. "
+  exit 1
+fi
+
+echo "🚀 Chạy ECS task với Docker image: $DOCKER_IMAGE"
+
+aws ecs run-task \
+  --cluster "$CLUSTER_NAME" \
+  --launch-type FARGATE \
+  --network-configuration "awsvpcConfiguration={subnets=[$SUBNET_1,$SUBNET_2],securityGroups=[$SECURITY_GROUP],assignPublicIp=ENABLED}" \
+  --task-definition "$TASK_DEF_NAME" \
+  --region "$REGION"
+i
